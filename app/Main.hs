c88a6f75d4cdd15e14c0f4d45da6f5c069a5fc81
Cj-bc <cj.bc-sd@outlook.jp>
2019-10-17 17:54:05 +0900

    Change: app/Main.mainLoop> separate command parsing part into other function

--- app/Main.hs
+++ app/Main.hs
@@ -23,19 +23,20 @@ sampleDisk = Directory "root"
                     ]
                 ]
 
+commandMap :: String -> FSZipper -> Either FSError (FSZipper, String)
+commandMap "ls"             z = ls z
+commandMap ('c':'d':' ':xs) z = cd xs z
+commandMap "exit"           _ = Left ExitCalled
+commandMap "zipper"         z = Right (z, show z)
+commandMap ""               z = Right (z, "")
+commandMap _                _ = Left CommandNotFound
 
 
 mainLoop :: FSZipper -> IO ()
 mainLoop z = do
     putStr "$ "
     cmd <- getLine
-    let result = case cmd of
-                "ls" -> ls z
-                ('c':'d':' ':xs) -> cd xs z
-                "exit" -> Left ExitCalled
-                "zipper" -> Right (z, show z)
-                "" -> Right (z, "")
-                _ -> Left CommandNotFound
+    let result = commandMap cmd z
         resText = case result of
                     Left e -> show e
                     Right (_, t) -> t
