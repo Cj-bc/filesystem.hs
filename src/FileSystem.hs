e29ff99caac943cf2eebda244dc2b140470d9aa4
Cj-bc <cj.bc-sd@outlook.jp>
2019-10-17 16:27:05 +0900

    Fix: src/FileSystem> cd won't fail when no Path is given
    This will make those codes work:
    ```
    cd ..
    cd /
    ```

--- src/FileSystem.hs
+++ src/FileSystem.hs
@@ -14,6 +14,7 @@ import  FileSystem.Internal
 -- | Move to given path
 -- This works as sh builtin 'cd' command (I hope)
 cd :: Path -> FSZipper -> Either FSError (FSZipper, String)
+cd [] z             = return (z, "")
 cd ('.':'.':ps) z   = fsUp z >>= cd ps
 cd ('/':ps) z       = fsUpToRoot z >>= cd ps
 cd ps z             = cdTo (splitOn "/" ps) z >>= \nz -> return (nz, "")
